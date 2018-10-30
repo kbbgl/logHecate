9715f898170732718b0cad928f7234fe37382f10
Kobbi Gal <kgwack@gmail.com>
2018-10-30 22:33:20 +0200

    added all event listening

--- src/main/java/IISDirectoryWatcher.java
+++ src/main/java/IISDirectoryWatcher.java
@@ -6,9 +6,8 @@ import java.nio.file.*;
 import java.util.stream.Stream;
 
 import static com.sun.jmx.mbeanserver.Util.cast;
-import static java.nio.file.StandardWatchEventKinds.OVERFLOW;
 
-public class IISDirectoryWatcher {
+class IISDirectoryWatcher {
 
     private Path path;
     private WatchService watchService;
@@ -24,19 +23,32 @@ public class IISDirectoryWatcher {
 
     private void printListFilesInDir() {
 
-        System.out.println("Files in " + this.path + ":");
-        try(Stream<Path> paths = Files.walk(Paths.get(path.toAbsolutePath().toString()))){
-            paths.filter(Files::isRegularFile)
-                    .forEach(System.out::println);
-        } catch (IOException e) {
-            e.printStackTrace();
+        File[] files = this.path.toFile().listFiles();
+
+        if (files != null && files.length > 0) {
+
+            System.out.println("Files in " + this.path + ":");
+
+            for (File file : files) {
+
+                if (file.isFile()){
+                    System.out.println("\t" + file.getName());
+                }
+            }
+        }
+        else {
+            System.out.printf("No files found in %s\n", this.path);
         }
 
     }
 
     private void register() throws IOException {
-        this.key = this.path.register(watchService, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_MODIFY);
-        System.out.printf("path %s registered\n", this.path);
+        this.key = this.path.
+                register(
+                        watchService,
+                        StandardWatchEventKinds.ENTRY_CREATE,
+                        StandardWatchEventKinds.ENTRY_MODIFY,
+                        StandardWatchEventKinds.ENTRY_DELETE);
     }
 
     private void processEvents(){
@@ -47,28 +59,45 @@ public class IISDirectoryWatcher {
                 for (WatchEvent<?> event : key.pollEvents()){
                     WatchEvent.Kind kind = event.kind();
 
-                    if (kind == OVERFLOW){
+                    if (kind == StandardWatchEventKinds.OVERFLOW){
+                        System.err.println("Overflow occurred");
                         continue;
                     }
 
                     WatchEvent<Path> ev = cast(event);
-                    Path filename = ev.context();
-                    Path absPath = Paths.get(this.path.toString(), filename.toString());
+                    handleEvent(ev);
 
-                    String extension = FilenameUtils.getExtension(filename.getFileName().toString());
-
-                    if (extension.equals("txt")){
-                        System.out.format("%s: %s\n", event.kind().name(), filename);
-                        readFileContent(absPath);
-                    }
-
-                    key.reset();
 
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    private void handleEvent(WatchEvent<Path> event){
+
+        Path filename = event.context();
+        Path absPath = Paths.get(this.path.toString(), filename.toString());
+        String extension = FilenameUtils.getExtension(filename.getFileName().toString());
+
+        if (extension.equals("txt")){
+            System.out.format("%s: %s\n", event.kind().name(), filename);
+            readFileContent(absPath);
+        }
+
+
+        if (event.kind() == StandardWatchEventKinds.ENTRY_CREATE){
+            System.out.printf("File created %s\n", event.context());
+        }
+        else if (event.kind() == StandardWatchEventKinds.ENTRY_DELETE){
+            System.out.printf("File deleted %s\n", event.context());
+        }
+        else if(event.kind() == StandardWatchEventKinds.ENTRY_MODIFY){
+            System.out.printf("File modified %s\n", event.context());
+        }
+
+        key.reset();
     }
 
     private void readFileContent(Path path){
